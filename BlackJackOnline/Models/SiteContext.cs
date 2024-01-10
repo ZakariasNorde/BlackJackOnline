bfeba02f717265d12540a08dae90d4fc3507be76
ZakariasNorde <132557167+ZakariasNorde@users.noreply.github.com>
2024-01-10 21:05:07 +0100

    Initial

---
+++ BlackJackOnline/Models/SiteContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+namespace BlackJackOnline.Models
+{
+	public class SiteContext : IdentityDbContext<User>
+	{
+		public SiteContext(DbContextOptions<SiteContext> options) : base(options) { }
+	}
+}
